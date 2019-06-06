6f7d08a67c07c69f1f88076efc99ee652cc0fc19
void-spark <81029971+void-spark@users.noreply.github.com>
2019-06-06 14:58:18 +0200

    Change esp32-idf image name

--- make_all.cmd
+++ make_all.cmd
@@ -1,2 +1,2 @@
 @ECHO OFF
-docker run -it --rm -v "%~dp0..:/esp/project" -w /esp/project esp32-idf bash -c "make -j8 all"
+docker run -it --rm -v "%~dp0..:/esp/project" -w /esp/project void-spark/esp32-idf bash -c "make -j8 all"
--- make_clean.cmd
+++ make_clean.cmd
@@ -1,2 +1,2 @@
 @ECHO OFF
-docker run -it --rm -v "%~dp0..:/esp/project" -w /esp/project esp32-idf bash -c "rm -rf build && rm sdkconfig && make defconfig && make -j8 clean"
+docker run -it --rm -v "%~dp0..:/esp/project" -w /esp/project void-spark/esp32-idf bash -c "rm -rf build && rm sdkconfig && make defconfig && make -j8 clean"
