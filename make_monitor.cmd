be4943d0e69b8aef8d4e46589305efcf04fc668b
void-spark <81029971+void-spark@users.noreply.github.com>
2020-07-05 14:00:08 +0200

    Correctly call monitor (doesn't seem to use sdkconfig port anymore)

--- make_monitor.cmd
+++ make_monitor.cmd
@@ -1,2 +1,2 @@
 @ECHO OFF
-docker run -it --rm -v "%~dp0..:/project" -w /project espressif/idf bash -c "export PYTHONPATH=/project/buildscripts:$PYTHONPATH && pushd /opt/esp/idf/components/esptool_py/esptool/ && git checkout v2.6 && patch -u esptool.py /project/buildscripts/esptool.py.patch && popd && idf.py monitor"
+docker run -it --rm -v "%~dp0..:/project" -w /project espressif/idf bash -c "idf.py monitor -p rfc2217://host.docker.internal:2217"
